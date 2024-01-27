7269ec97ca2b5b0e40b248ef114305384dcce140
lilbbusa <bbostergames@gmail.com>
2024-01-27 13:03:04 -0600

    Created Wake Up Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,4 +32,9 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+    public void LoadWakeUpScene()
+    {
+        SceneManager.LoadScene("WakeUp");
+    }
 }
