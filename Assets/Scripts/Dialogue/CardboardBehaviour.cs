d8f2a9569672bb51f89f03e7f9a401befa840e17
bee <104741505+TheBeesSneeze@users.noreply.github.com>
2024-01-27 18:01:22 -0600

    yea of course i can

---
+++ Assets/Scripts/Dialogue/CardboardBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CardboardBehaviour : DialogueNPCBehaviour
+{
+    public override void ActivateSpeech(InputAction.CallbackContext obj)
+    {
+        //Animator.
+    }
+}
